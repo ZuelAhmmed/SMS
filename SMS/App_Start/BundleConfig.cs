624e0d1dabd8643f33c33533d81ad52d4f9bb966
ZuelAhmmed <zuel.ahmed@yahoo.com>
2016-08-20 23:45:12 +0600

    Date Picker Add for SMS Project

--- SMS/App_Start/BundleConfig.cs
+++ SMS/App_Start/BundleConfig.cs
@@ -20,13 +20,15 @@ namespace SMS
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                       "~/Scripts/bootstrap.js",
+                       "~/Scripts/bootstrap-datepicker.js",
                       "~/Scripts/respond.js",
                       "~/Scripts/DataTables/jquery.dataTables.min.js"
                       ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-datepicker.css",
                       "~/Content/site.css",
                       "~/Content/DataTables/css/jquery.dataTables.min.css"
                       ));
